6282db0ad3ef2e7a244d95923b62aa44cc59e104
Kamil Malinowski <malinowskikamil91@gmail.com>
2018-10-08 22:34:06 +0200

    removed unused variables

--- src/containers/Series/index.jsx
+++ src/containers/Series/index.jsx
@@ -17,12 +17,11 @@ class Series extends Component {
     let randomSeries = this.state.randomSeries;
     fetch(`https://api.tvmaze.com/shows/${randomNumber}?embed=episodes`).then(
       response => {
-        let countSeries = this.state.countSeries;
         if (response.ok) {
           response.json().then(json => {
             if (json.image !== null) {
               randomSeries.push(json);
-              this.setState({ randomSeries, countSeries });
+              this.setState({ randomSeries });
               randomSeries.length === 10 &&
                 this.setState({ isLoadedSeries: true });
             } else {
@@ -39,12 +38,11 @@ class Series extends Component {
     let randomNumber = Math.floor(Math.random() * 10000 + 1);
     let randomActors = this.state.randomActors;
     fetch(`https://api.tvmaze.com/people/${randomNumber}`).then(response => {
-      let countActors = this.state.countActors;
       if (response.ok) {
         response.json().then(json => {
           if (json.image !== null) {
             randomActors.push(json);
-            this.setState({ randomActors, countActors });
+            this.setState({ randomActors });
             randomActors.length === 10 &&
               this.setState({ isLoadedActors: true });
           } else {
